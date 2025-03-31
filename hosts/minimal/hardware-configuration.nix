5c2e91ee7e9e809ac371739fbf365e7c46464375
Mistereptil09 <antoniociorba@outlook.com>
2025-03-31 15:15:38 +0200

    hardware placeholder will now stop the compile if not changed

--- hosts/minimal/hardware-configuration.nix
+++ hosts/minimal/hardware-configuration.nix
@@ -2,26 +2,22 @@
 
 let
   isPlaceholder = true; # Set this to false after properly configuring your hardware
+  errorMsg = ''
+    ⚠️ ERROR: You need to generate a proper hardware-configuration.nix file!
+    
+    Please do one of the following:
+    1. Run 'sudo nixos-generate-config' on your system and copy the generated
+       hardware-configuration.nix to this file
+    2. Or manually configure the hardware settings and set isPlaceholder = false
+    
+    This file contains PLACEHOLDER values that will NOT work on a real system.
+  '';
 in
 {
-  imports = [ 
-    # Use NixOS hardware scanning module
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  # Stop the build if this file hasn't been properly configured
-  _module.check = builtins.trace (
-    if isPlaceholder then ''
-      ⚠️ ERROR: You need to generate a proper hardware-configuration.nix file!
-      
-      Please do one of the following:
-      1. Run 'sudo nixos-generate-config' on your system and copy the generated
-         hardware-configuration.nix to this file
-      2. Or manually configure the hardware settings and set isPlaceholder = false
-      
-      This file contains PLACEHOLDER values that will NOT work on a real system.
-    '' else ""
-  ) (!isPlaceholder);
+  imports = 
+    if isPlaceholder 
+    then throw errorMsg
+    else [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   # Let the system detect your hardware
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
