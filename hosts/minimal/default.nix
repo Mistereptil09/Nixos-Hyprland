dc3aa88824c47f4d8de2b9bf2f0f573fc61416db
Mistereptil09 <antoniociorba@outlook.com>
2025-04-02 18:55:09 +0200

    name priorities love to see it...

--- hosts/minimal/default.nix
+++ hosts/minimal/default.nix
@@ -6,9 +6,18 @@
   
   networking.hostName = "minimal";
   
+<<<<<<< HEAD
   # User is configured via modules.core.user
   user.name = "antonio";
   
+=======
+  # Use mkForce to ensure this definition takes precedence
+  user.name = lib.mkForce "antonio";
+  initialPassword = lib.mkForce "root";
+  user.isNormalUser = true;
+  user.extraGroups = [ "wheel" "networkmanager" ];
+
+>>>>>>> 9bbf011 (name priorities love to see it...)
   # Home-manager imports the user config
   home-manager.users.antonio = import ./home.nix;
 }
