d69f48d11d934beeb2c0ea925021bc87ea28ce46
Mistereptil09 <antoniociorba@outlook.com>
2025-04-02 19:30:58 +0200

    again

--- modules/core/boot/default.nix
+++ modules/core/boot/default.nix
@@ -10,19 +10,11 @@ with lib;
   config = mkIf config.core.boot.enable {
     # Boot loader configuration
     boot.loader = {
-      grub.devices = [ "/vda/vda1" ]; # Replace with your boot disk
-      systemd-boot = {
+      grub = {
         enable = true;
+        device = "nodev";       # Use "nodev" when targeting a specific disk
+        devices = [ "/dev/vda" ]; # For VMs, install to the whole disk
       };
-      # efi = {
-      #   canTouchEfiVariables = true;
-      #   efiSysMountPoint = "/boot";  # Make sure this matches your boot partition
-      # };
-      # Explicitly disable GRUB to avoid conflicts
-      grub.enable = true;
-      
     };
-    
-    # Add other boot-related settings
   };
 }
